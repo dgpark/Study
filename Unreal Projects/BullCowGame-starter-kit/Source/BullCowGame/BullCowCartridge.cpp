2124db7045bccb38a9e2e763019700e579c85208
unknown <dgpark220@gmail.com>
2020-08-11 22:22:38 +0900

    Unreal C++ Study Project Start

---
+++ Unreal Projects/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+
+    // Welcoming The Player
+    PrintLine(TEXT("Welcome to Bull Cows!"));
+    PrintLine(TEXT("Guess the 4 letter word!")); // Magic Number Remove!
+    PrintLine(TEXT("Press enter to continue..."));
+    
+    // Setting Up Game
+    HiddenWord = TEXT("cake"); // Set the HiddenWord
+
+    // Set lives
+
+    // Prompt Player For Guess
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+
+    // Checking PlayerGuess
+
+    if (Input == HiddenWord) {
+        PrintLine(TEXT("You have Won!"));
+    }
+    else {
+        PrintLine(TEXT("You have Lost!"));
+
+    }
+    // Check If Isogram
+    // Prompt To Guess Again
+    // Check Right Number of Characters
+    // Prompt To Guess Again
+
+    // Remove Life
+
+    // Check If Lives > 0
+    // If Yes Guess Again
+    // Show Lives Left
+    // If No Show GameOver and HiddenWord?
+    // Prompt To Play Again, Press Enter To Play Again?
+    // Check User Input
+    // Play Again Or Quit
+}
