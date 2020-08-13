0aade6eace9f454c650a01ea44a0662b4ec4dd07
unknown <dgpark220@gmail.com>
2020-08-13 21:13:34 +0900

    Unreal MiniProject Study Finish

--- Unreal Projects/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
+++ Unreal Projects/BullCowGame-starter-kit/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,23 +1,24 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
+#include "HiddenWordList.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
 
-    SetupGame();
+    Isograms = GetValidWords(Words);
 
-    PrintLine(FString::Printf(TEXT("The Hidden Word is %s"), *HiddenWord)); // Debug Line
+    SetupGame();
 }
 
-void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+void UBullCowCartridge::OnInput(const FString& PlayerInput) // When the player hits enter
 {
     if (bGameOver) {
         ClearScreen();
         SetupGame();
     }
     else { // Checking PlayerGuess
-        ProcessGuess(Input);
+        ProcessGuess(PlayerInput);
     }
 }
 
@@ -25,11 +26,12 @@ void UBullCowCartridge::SetupGame() {
     // Welcoming The Player
     PrintLine(TEXT("Welcome to Bull Cows!"));
 
-    HiddenWord = TEXT("cakes");
-    Lives = HiddenWord.Len();
+    HiddenWord = Isograms[FMath::RandRange(0,Isograms.Num()-1)];
+    Lives = HiddenWord.Len()*2;
     bGameOver = false;
 
     PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("The Hidden Word is %s"), *HiddenWord);
     PrintLine(TEXT("You have %i lives"), Lives);
     PrintLine(TEXT("Type in your guess \npress enter to continue...")); // Prompt Player For Guess
 }
@@ -39,23 +41,26 @@ void UBullCowCartridge::EndGame() {
     PrintLine(TEXT("\nPress enter to play again."));
 }
 
-void UBullCowCartridge::ProcessGuess(FString Guess) {
+void UBullCowCartridge::ProcessGuess(const FString& Guess) {
     if (Guess == HiddenWord) {
         PrintLine(TEXT("You have Won!"));
         EndGame();
         return;
     }
 
-    //// Check If Isogram
-    //if (!IsIsogram) {
-    //    PrintLine(TEXT("No repeating letters, guess again"));
-    //}
-
     if (Guess.Len() != HiddenWord.Len()) {
         PrintLine(TEXT("The hidden word is %i letters long"), HiddenWord.Len());
         PrintLine(TEXT("Sorry, try guessing again! \nyou have %i lives remaining"), Lives);
         return;
     }
+
+    // Check If Isogram
+    if (!IsIsogram(Guess)) {
+
+        PrintLine(TEXT("No repeating letters, guess again!"));
+        return;
+    }
+
     // Remove Life
     PrintLine(TEXT("Lost a life!"));
     --Lives;
@@ -68,5 +73,51 @@ void UBullCowCartridge::ProcessGuess(FString Guess) {
         return;
     }
     // Show the player Bulls and Cows
+    FBullCowCount Score = GetBullCows(Guess);
+
+    PrintLine(TEXT("You have %i Bulls and %i Cows"), Score.Bulls, Score.Cows);
+
     PrintLine(TEXT("Guess again, you have %i lives left"), Lives);
+}
+
+bool UBullCowCartridge::IsIsogram(const FString& Word) const {
+    
+    for (int32 Index = 0; Index < Word.Len(); ++Index) {
+        for (int32 Comparison = Index + 1; Comparison < Word.Len(); ++Comparison) {
+            if (Word[Index] == Word[Comparison]) {
+                return false;
+            }
+        }
+    }
+    
+    return true;
+}
+
+TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& WordList) const {
+    TArray<FString> ValidWords;
+
+    for (FString Word : WordList) {
+        if (Word.Len() >= 4 && Word.Len() <= 8 && IsIsogram(Word)) {
+            ValidWords.Emplace(Word);
+        }
+    }
+    return ValidWords;
+}
+
+FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const {
+    FBullCowCount Count;
+
+    for (int32 GuessIndex = 0; GuessIndex < Guess.Len(); ++GuessIndex) {
+        if (Guess[GuessIndex] == HiddenWord[GuessIndex]) {
+            ++Count.Bulls;
+            continue;
+        }
+
+        for (int32 HiddenIndex = 0; HiddenIndex < HiddenWord.Len(); ++HiddenIndex){
+            if (Guess[GuessIndex] == HiddenWord[HiddenIndex]) {
+                ++Count.Cows;
+            }
+        }
+    }
+    return Count;
 }
